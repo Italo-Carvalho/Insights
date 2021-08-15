1a178c3d9c4d7ea4206c7440640baed3b6b3976a
Ítalo Carvalho <italocarvalhoti@hotmail.com>
2021-08-14 22:51:29 -0300

    remove db test

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 @pytest.fixture(scope="session")
 def django_db_setup():
-    settings.DATABASES["teste"] = {
+    settings.DATABASES["default"] = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
         "USER": "postgres",
